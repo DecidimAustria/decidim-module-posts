5b18f4e74110ef70c0de65fc739e7be5f3fae4a8
Alexander Rusa <alex@rusa.at>
2024-08-18 13:29:28 +0000

    Duplicate everything from decidim-feeds module

---
+++ app/commands/decidim/feeds/destroy_post.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module Decidim
+  module Feeds
+    class DestroyPost < Decidim::Command
+      def initialize(post, current_user)
+        @post = post
+        @current_user = current_user
+      end
+
+      def call
+        return broadcast(:invalid) unless @post.authored_by?(@current_user)
+
+        @post.destroy!
+
+        broadcast(:ok, @post)
+      end
+    end
+  end
+end
---
+++ app/packs/entrypoints/decidim_feeds.js
@@ -0,0 +1,5 @@
+import 'src/decidim/feeds/feeds.js';
+
+require.context('../images', true);
+
+import 'entrypoints/decidim_feeds.scss';
