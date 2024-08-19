11f7673fce827efd41f89f67d79b53db1c69918f
Alexander Rusa <alex@rusa.at>
2024-08-19 18:29:32 +0000

    Fix modal scrolling

--- app/packs/entrypoints/decidim_feeds.js
+++
@@ -1,5 +0,0 @@
-import 'src/decidim/posts/feeds.js';
-
-require.context('../images', true);
-
-import 'entrypoints/decidim_feeds.scss';
---
+++ app/packs/entrypoints/decidim_posts.js
@@ -0,0 +1,5 @@
+import 'src/decidim/posts/posts.js';
+
+require.context('../images', true);
+
+import 'entrypoints/decidim_posts.scss';
