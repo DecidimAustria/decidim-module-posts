932c379eac7bb165038ae580d6e20fe26fabc990
webchroma <info@webchroma.de>
2024-12-03 13:48:12 +0000

    posts content block

--- app/cells/decidim/posts/content_blocks/posts_cell.rb
+++ app/cells/decidim/posts/content_blocks/posts_cell.rb
@@ -5,21 +5,20 @@ module Decidim
     module ContentBlocks
       class PostsCell < Decidim::ViewModel
         include Cell::ViewModel::Partial
-        include FormFactory
-
+        include PostCellsHelper
         def show
           @posts = Decidim::Posts::Post
                    .where(decidim_component_id: component_id)
                    .filter_category(params[:filter_post_category])
                    .order(created_at: :desc)
-                   .limit(10)
+                   .limit(6)
           extra_context = {
             current_component: component,
             current_organization: component.organization,
             current_user:,
             current_participatory_space: component.participatory_space
           }
-          @form = form(Decidim::Posts::PostForm).from_params(params, extra_context)
+          # @form = form(Decidim::Posts::PostForm).from_params(params, extra_context)
           render :show
         end
 
@@ -41,9 +40,9 @@ module Decidim
           @component ||= Decidim::Component.find(component_id)
         end
 
-        def post_creation_params
-          params[:post]
-        end
+        # def post_creation_params
+        #   params[:post]
+        # end
       end
     end
   end
--- lib/decidim/posts/content_blocks/content_blocks_homepage.rb
+++ lib/decidim/posts/content_blocks/content_blocks_homepage.rb
@@ -2,18 +2,22 @@
 
 def initialize_homepage_content_blocks
   initializer "Feeds.content_blocks" do
-    Decidim.content_blocks.register(:homepage, :feeds) do |content_block|
-      content_block.cell = "decidim/posts/content_blocks/feeds"
-      content_block.settings_form_cell = "decidim/posts/content_blocks/feeds_settings_form"
-      content_block.public_name_key = "decidim.posts.content_blocks.feeds.name"
+    register_content_block(:homepage, :feeds_homepage)
+    register_content_block(:assembly_homepage, :feeds_assembly_homepage)
+    register_content_block(:participatory_process_homepage, :feeds_participatory_process_homepage)
+    register_content_block(:participatory_process_group_homepage, :feeds_participatory_process_group_homepage)
+  end
+end
 
-      content_block.settings do |settings|
-        # settings.attribute :title, type: :text, translated: true
-        # settings.attribute :link_text, type: :text, translated: true
-        # settings.attribute :link_url, type: :text, translated: true
-        # settings.attribute :count, type: :integer, default: 3
-        settings.attribute :component_id, type: :integer
-      end
+
+def register_content_block(component, type)
+  Decidim.content_blocks.register(component, type) do |content_block|
+    content_block.cell = "decidim/posts/content_blocks/posts"
+    content_block.settings_form_cell = "decidim/posts/content_blocks/posts_settings_form"
+    content_block.public_name_key = "decidim.posts.content_blocks.posts.name"
+
+    content_block.settings do |settings|
+      settings.attribute :component_id, type: :integer
     end
   end
 end
