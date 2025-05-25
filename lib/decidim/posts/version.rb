9255c1411d7b4048ceee162c00e0ea11c57cf19b
Alexander Rusa <alex@rusa.at>
2025-05-25 16:36:26 +0000

    Fix form paths for feeds-ui
    This fixes a bug when you are redirected to change your password while
    the footer contains the new post form

--- lib/decidim/posts/version.rb
+++ lib/decidim/posts/version.rb
@@ -3,7 +3,7 @@
 module Decidim
   # This holds the decidim-meetings version.
   module Posts
-    VERSION = "2.0.0"
+    VERSION = "2.0.1"
     COMPAT_DECIDIM_VERSION = [">= 0.28.0", "< 0.31"].freeze
   end
 end
